0adfdfb474bbf10403e3a29ceeab22287ab93432
gustavo <gustavo.ubalperez@gmail.com>
2022-01-20 12:55:01 -0300

    carousel added

--- assets/js/main.js
+++ assets/js/main.js
@@ -179,6 +179,17 @@ function formError(){
     }
   }, true)
 
+  //  Hero carousel indicators
+
+let heroCarouselIndicators = select("#hero-carousel-indicators")
+let heroCarouselItems = select('#heroCarousel .carousel-item', true)
+
+heroCarouselItems.forEach((item, index) => {
+ (index === 0) ?
+ heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
+   heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
+});
+
   /**
    * Scrool with ofset on links with a class name .scrollto
    */
